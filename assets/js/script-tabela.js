900860e263543da17401def5199f4a55236fa22d
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-06-26 10:22:24 -0400

    doc: att

--- assets/js/script-tabela.js
+++ assets/js/script-tabela.js
@@ -78,17 +78,21 @@ function addHeaderCell(tabela, cabecalho) {
     const nomeTabela = tabela.id;
     const thead = tabela.querySelector("thead tr");
     const th = document.createElement("th");
-    th.id = cabecalho;
+    th.id = nomeTabela + "-" + cabecalho;
 
     th.addEventListener("mouseenter", function() {
-        const divTh = document.querySelector("#" + cabecalho);
+        
+
+        console.log(nomeTabela);
+
+        const divTh = document.querySelector("#" + nomeTabela + "-" + cabecalho);
         divTh.style.border = "1px solid #e9e9e9";
         divTh.style.boxsizing = "border-box";
         divTh.style.backgroundColor = "#fafafa";  
     })
 
     th.addEventListener("mouseleave", function() {
-        const divTh = document.querySelector("#" + cabecalho);
+        const divTh = document.querySelector("#" + nomeTabela + "-" + cabecalho);
         divTh.style.border = "none";
         divTh.style.boxsizing = "border-box";
         divTh.style.backgroundColor = "white";
